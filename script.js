50cfd5a8477fca23f3ce9cc83b0d2ef535c1e3d5
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-09-02 11:21:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+//const numeros = [50, 45, 67, 89, 10, 5]
+
+//const multiplicarPorDois = numeros.map (numero => numero * 2);
+
+//console.log(multiplicarPorDois)
+
+//console.log(numeros)
+
+//---- QUESTÃO 2
+
+//const pares = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+  //  const exibirNumerosPares = pares.map(numero => numero % 2 === 0)
+
+    //console.log(pares)
+    //console.log(exibirNumerosPares)
+
+   // --- QUESTÃO 3
+
+   //const numeros = [1 ,2, 3, 4, 5, 6]
+    
+   // const numerosSomados = numeros.map (numeros => numeros + 1 ,2, 3, 4, 5, 6);
+
+    //console.log(numeros)
+    //console.log(numerosSomados)
+
+    //----- QUESTÃO 4
+
+      //const numeros = [1 ,2, 3, 4, 5, 6]
+
+      //const quadrado = numeros (numeros => numeros * numeros)
+
+      //console.log(numeros)
+      //console.log(quadrado)
+
+      //---- QUESTÃO 5
+
+     // const lista = ["maçã", "banana", "cereja"];
+
+      //const novaLista = lista.map(str => str.toUpperCase());
+
+      //console.log(novaLista);
+
+      //  --- QUESTÃO SEIS
+
+  const lista = ["maçã", "banana", "cereja", "kiwi", "manga"];
+    
+  const novaLista = lista.filter(str => str.length > 5);
+  
+  console.log(novaLista);
